32676885a3774f363e7664628a79f8505f82ad44
fletcher <fletcherupchurch@gmail.com>
2016-04-20 13:28:58 -0400

    fixed migration file

--- db/migrate/20160420172813_add_values_to_user.rb
+++ db/migrate/20160420172813_add_values_to_user.rb
@@ -1,7 +1,7 @@
-class AddValuesToUsers < ActiveRecord::Migration
+class AddValuesToUser < ActiveRecord::Migration
   def change
     add_column :users, :name, :string
+    add_column :users, :birthday, :date
     add_column :users, :username, :string
-    add_column :users, :birthday, :email
   end
 end
